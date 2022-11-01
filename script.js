7538220e53288636de791d8e63312deb16492a5f
Vineethkumar-Shetty <shettyvineethkumar@gmail.com>
2022-11-02 00:39:00 +0530

    style: added styles

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 var birthdayDate = document.querySelector("#bd-date");
 var luckyNumber = document.querySelector("#lucky-number");
 var checkButton = document.querySelector("#check-btn");
-var messageDispay = document.querySelector("#output-text")
+var messageDispay = document.querySelector("#output-text");
 
-
-function luckyLogic (sum,luckyNumber) {
-    if(sum%luckyNumber ==0) {
-        messageDispay.innerText = "Your birthday is lucky 🚀";
-    }else {
-        messageDispay.innerText = "Your birthday is not lucky 🙅‍♂️";
-    }
+function luckyLogic(sum, luckyNumber) {
+  if (sum % luckyNumber == 0) {
+    messageDispay.innerText = "Your birthday is lucky 🚀";
+    messageDispay.style.color = "#4cbb17";
+  } else {
+    messageDispay.innerText = "Your birthday is not lucky 🙅‍♂️";
+    messageDispay.style.color = "red";
+  }
 }
 
 function isBirtdateLucky() {
-    var sum = calculateSum(birthdayDate.value);
-    if (sum&&luckyNumber.value) {
-        luckyLogic(sum,luckyNumber.value);
-    }
-    else {
-        messageDispay.innerText = "Please enter both the fiels😠";
-    }
+  var sum = calculateSum(birthdayDate.value);
+  if (sum && luckyNumber.value) {
+    luckyLogic(sum, luckyNumber.value);
+  } else {
+    messageDispay.innerText = "Please enter both the fiels😠";
+    messageDispay.style.color = "yellow";
+  }
 }
 
 function calculateSum(bdDate) {
-    bdDate = bdDate.replaceAll("-","");
-    let sum = 0;
-    for (i=0; i<bdDate.length;i++) {
-        sum = sum + Number(bdDate.charAt(i));
-    }
-    return sum;
+  bdDate = bdDate.replaceAll("-", "");
+  let sum = 0;
+  for (i = 0; i < bdDate.length; i++) {
+    sum = sum + Number(bdDate.charAt(i));
+  }
+  return sum;
 }
-checkButton.addEventListener("click",isBirtdateLucky)
+checkButton.addEventListener("click", isBirtdateLucky);
